632f374e239f995a7b8e6f45b299939577bc85b5
Farhan209 <farhan209@hotmail.com>
2021-08-05 11:49:10 +0100

    P8 update

--- Problem8/Program.cs
+++ Problem8/Program.cs
@@ -12,45 +12,30 @@ namespace Problem8
         {
             int[] list = { 3, 1, 5, 7, 5, 9};
             int target = 10;
-            int[] result = TwoSum(list, target);
+            int[] result = TwoSums(list, target);
             Console.WriteLine("[" + list[result[0]] + ", " + list[result[1]] + "]");
             Console.ReadLine();
         }
 
-        public static int[] TwoSums(int[] nums, int target) {
+        public static int[] TwoSums(int[] nums, int target)
+        {
             int[] result = new int[2];
             var dict = new Dictionary<int, int>();
-            for (int i = 0; i < nums.Length; i++) {
+            for (int i = 0; i < nums.Length; i++)
+            {
                 int goal = target - nums[i];
-                if (dict.ContainsKey(goal)) {
+                if (dict.ContainsKey(goal))
+                {
                     result[1] = i;
                     result[0] = dict[goal];
                     break;
-                } else if (!dict.ContainsKey(nums[i])){
+                }
+                else if (!dict.ContainsKey(nums[i]))
+                {
                     dict.Add(nums[i], i);
                 }
             }
             return result;
-        }
-
-        public static int[] TwoSum(int[] nums, int target)
-        {
-            int[] answer = new int[2];
-
-            Dictionary<int, int> myDict = new Dictionary<int, int>();
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (myDict.ContainsKey(target - nums[i]))
-                {
-                    answer[1] = i;
-                    answer[0] = myDict[target - nums[i]];
-                    break;
-                }
-
-                else if (!myDict.ContainsKey(nums[i])) myDict.Add(nums[i], i);
-            }
-
-            return answer;
         }
     }
 }
